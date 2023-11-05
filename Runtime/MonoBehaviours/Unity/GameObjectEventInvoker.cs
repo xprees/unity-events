f868552609323bcccdedd07eb6765afce88f39be
Vaclav Bily <xbily@fi.muni.cz>
2023-11-05 19:18:17 +0100

    MonoBehaviour OnValidate run only on instantiated instance (Not prefabs)

--- Runtime/MonoBehaviours/Unity/GameObjectEventInvoker.cs
+++ Runtime/MonoBehaviours/Unity/GameObjectEventInvoker.cs
@@ -18,6 +18,8 @@ namespace Xprees.Events.MonoBehaviours.Unity
 #if UNITY_EDITOR
         private void OnValidate()
         {
+            // Check that the GameObject is present in the scene and not a prefab
+            if (gameObject.scene.rootCount == 0) return;
             if (gameObjectEvent == null) Debug.LogWarning($"{nameof(GameObjectEventInvoker)}: GameObjectEvent is not set", gameObject);
             if (target == null) Debug.LogWarning($"{nameof(GameObjectEventInvoker)}: GameObject Data is not set", gameObject);
         }
