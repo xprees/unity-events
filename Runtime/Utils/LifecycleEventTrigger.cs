34bda0196c2050eea1afb4baece754a7e2985176
Vaclav Bily <xbily@fi.muni.cz>
2025-04-17 23:13:46 +0200

    Add Utils - LifecycleEventTrigger

---
+++ Runtime/Utils/LifecycleEventTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Xprees.Events.ScriptableObjects.Base;
+
+namespace Xprees.Events.Utils
+{
+    public class LifecycleEventTrigger : MonoBehaviour
+    {
+        [Header("Broadcasting Events")]
+        public VoidEventChannelSO onAwake;
+
+        public VoidEventChannelSO onStart;
+        public VoidEventChannelSO onEnable;
+        public VoidEventChannelSO onDisable;
+        public VoidEventChannelSO onDestroy;
+
+        private void Awake() => onAwake?.RaiseEvent();
+
+        private void Start() => onStart?.RaiseEvent();
+
+        private void OnEnable() => onEnable?.RaiseEvent();
+
+        private void OnDisable() => onDisable?.RaiseEvent();
+
+        private void OnDestroy() => onDestroy?.RaiseEvent();
+
+    }
+}
